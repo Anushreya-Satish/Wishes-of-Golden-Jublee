e4f4968a07ec9e3fe390f92923d9dbecb7f49dbd
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2021-07-01 22:27:23 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,16 @@
+playSound("assets/Adrienette-Dance-Melody.mp3", true);
+onEvent("button1", "click", function( ) {
+	setScreen("screen2");
+});
+onEvent("image6", "click", function( ) {
+	setScreen("screen3");
+});
+onEvent("image4", "click", function( ) {
+	setScreen("screen5");
+});
+onEvent("image2", "click", function( ) {
+	setScreen("screen4");
+});
+onEvent("image9", "click", function( ) {
+	setScreen("screen6")
+});
